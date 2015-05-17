0591e96483cd2f8d9e68ef8b0811ebf2e49ed12e
Gabe Kangas <gabek@real-ity.com>
2015-05-16 20:35:28 -0700

    Troubleshooting

--- modules/getTitleShoutcast.js
+++ modules/getTitleShoutcast.js
@@ -13,7 +13,7 @@ function getV1Title(url) {
 
     var options = {
       url: url,
-      timeout: 1200,
+      timeout: 5200,
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
       }
@@ -22,7 +22,7 @@ function getV1Title(url) {
     var res = request(options, function(error, response, body) {
       if (error || body === undefined) {
         log(error);
-        return reject(undefined);
+        return fulfill(undefined);
       }
 
       var csv = body.stripTags();
