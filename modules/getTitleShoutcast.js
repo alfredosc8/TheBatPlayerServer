4bae0970f19bb3d90e4fbb24c6e14647ea694a9b
Gabe Kangas <gabek@real-ity.com>
2015-05-16 20:37:24 -0700

    Troubleshooting

--- modules/getTitleShoutcast.js
+++ modules/getTitleShoutcast.js
@@ -13,7 +13,7 @@ function getV1Title(url) {
 
     var options = {
       url: url,
-      timeout: 5200,
+      timeout: 200,
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'
       }
