591978938e2044dab1fb9b45860069ab58428182
Gabe Kangas <gabek@real-ity.com>
2015-05-31 18:33:45 -0700

    Updated cache control headers

--- routes/backgroundImage.js
+++ routes/backgroundImage.js
@@ -6,7 +6,13 @@ module.exports = (function() {
   var router = express.Router();
 
   router.get("/:imageurl/:red/:green/:blue", function(req, res) {
-    res.setHeader('Cache-Control', 'public, max-age=31556926'); // one year
+    var expires = new Date(new Date().getTime() + (31556926 * 1000)).toUTCString();
+    res.setHeader('response-cache-control', 'maxage=31556926');
+    res.setHeader('Cache-Control', 'maxage=31556926');
+    res.setHeader('response-expires', expires);
+    res.setHeader('Expires', expires);
+    res.setHeader('x-amz-acl', 'public-read');
+
     res.writeHead(200, {
       'Content-Type': 'image/jpeg'
     });
