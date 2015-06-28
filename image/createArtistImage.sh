8bc54d921ab4eb89d81da4a1dff74c8c5f832abf
Gabe Kangas <gabek@real-ity.com>
2015-06-28 13:29:48 -0700

    Tweak artist image shadow a bit

--- image/createArtistImage.sh
+++ image/createArtistImage.sh
@@ -14,6 +14,6 @@ convert $1 \
 -trim \
 -alpha set -virtual-pixel transparent \
 -channel A -blur 0x8  -level 50%,100% +channel \
-\( +clone -background black -shadow 75x3+9+9 \) +swap \
+\( +clone -background black -shadow 75x3+9+10 \) +swap \
 -background none -compose Over -layers merge +repage \
 png:$3
