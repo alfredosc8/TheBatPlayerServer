985fe2514b8e1a8dca3b601a9aad6ef0e58ff8f1
Gabe Kangas <gabek@real-ity.com>
2015-07-16 08:52:36 -0700

    Use __dirname on header generator

--- image/header.js
+++ image/header.js
@@ -14,7 +14,7 @@ function createHeader(text, width, callback) {
       return;
     }
 
-    var command = "convert ./image/resources/selection_bat_logo-HD.png -strip -quality 90% -interlace Plane -background Transparent -stroke \"#F4B6AF\" -fill \"#F4B6AF\" -font ./image/resources/Calibri.ttf -pointsize 25 -annotate +380+140 \"" + text + " \" -resize " + width + "X200! jpg:" + path;
+    var command = "convert " + __dirname + "/resources/selection_bat_logo-HD.png -strip -quality 90% -interlace Plane -background Transparent -stroke \"#F4B6AF\" -fill \"#F4B6AF\" -font ./image/resources/Calibri.ttf -pointsize 25 -annotate +380+140 \"" + text + " \" -resize " + width + "X200! jpg:" + path;
 
     var child = exec(command, null, function(err, stdout, stderr) {
       log(command);
