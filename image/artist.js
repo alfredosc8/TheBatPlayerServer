1db8bd2d1ee30a6b9f2d239933e9f7abd1e0e7a6
Gabe Kangas <gabek@real-ity.com>
2015-11-30 14:31:53 -0800

    Trying to swap out image processing with imgix

--- image/artist.js
+++ image/artist.js
@@ -3,40 +3,57 @@ var utils = require("../utils/utils.js");
 var fs = require('fs');
 var config = require("../config.js");
 var log = utils.log;
+var ImgixClient = require('imgix-core-js');
+var client = new ImgixClient("thebatplayer.imgix.net", "XTuJHoqghB0BwXOH");
 
-function createArtistImage(url, colorObject, callback) {
+function createArtistImage(url, colorObject) {
+  var mask = "https://s3-us-west-2.amazonaws.com/batserver-static-assets/appletv-client/grunge-inverted.png";
+  var format = "png";
+  var colorString = "'rgb\(" + colorObject.red + "," + colorObject.green + "," +
+    colorObject.blue + "\)'";
 
-  var path = utils.getCacheFilepathForUrl(url, "artists");
-  var cacheFile = utils.getCacheFilepathForUrl(url, "original");
+  var url = client.path(url).toUrl({
+    w: 700,
+    h: 530,
+    blend: colorString,
+    bm: "color",
+    fm: format,
+    balph: 20
+  }).toString();
 
-  fs.exists(path, function(exists) {
-    if (exists && config.enableImageCache) {
-      callback(null, path);
-      return;
-    }
-
-    utils.download(url, cacheFile, function() {
-      var rgb = "'rgb\(" + colorObject.red + "," + colorObject.green + "," + colorObject.blue + "\)'";
-      var command = "/bin/bash " + __dirname + "/createArtistImage.sh " + cacheFile + " " + rgb + " " + path;
-      log(command);
-
-      var child = exec(command, null, function(err, stdout, stderr) {
-        if (!err && !stderr) {
-          callback(null, path);
-        } else {
-          utils.logError(err);
-          utils.logError(stderr);
-          if (err) {
-            throw err;
-          }
-          callback(stderr, cacheFile);
-        }
-      });
-
-    });
-  });
-
-
+  return url;
 }
+//   var path = utils.getCacheFilepathForUrl(url, "artists");
+//   var cacheFile = utils.getCacheFilepathForUrl(url, "original");
+//
+//   fs.exists(path, function(exists) {
+//     if (exists && config.enableImageCache) {
+//       callback(null, path);
+//       return;
+//     }
+//
+//     utils.download(url, cacheFile, function() {
+//       var rgb = "'rgb\(" + colorObject.red + "," + colorObject.green + "," + colorObject.blue + "\)'";
+//       var command = "/bin/bash " + __dirname + "/createArtistImage.sh " + cacheFile + " " + rgb + " " + path;
+//       log(command);
+//
+//       var child = exec(command, null, function(err, stdout, stderr) {
+//         if (!err && !stderr) {
+//           callback(null, path);
+//         } else {
+//           utils.logError(err);
+//           utils.logError(stderr);
+//           if (err) {
+//             throw err;
+//           }
+//           callback(stderr, cacheFile);
+//         }
+//       });
+//
+//     });
+//   });
+//
+//
+// }
 
 module.exports.createArtistImage = createArtistImage;
