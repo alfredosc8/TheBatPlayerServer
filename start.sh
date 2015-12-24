b355af26d5709cb8dc096a0895f0c400dc563786
Gabe Kangas <gabek@real-ity.com>
2015-12-24 12:40:08 -0800

    Reduce the allocated amount of ram for the node process

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ npm update node-internet-radio
 export NEW_RELIC_LOG=stdout
 
 totalRam=`vmstat --unit m -s |grep "total memory" |awk '{print $1;}'`
-assignedRam=`echo "$totalRam * 0.7" | bc -l`
+assignedRam=`echo "$totalRam * 0.6" | bc -l`
 roundedValue=${assignedRam%.*}
 
 echo "Starting The Bat Server with RAM: " $roundedValue "M"
