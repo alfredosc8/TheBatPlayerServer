10228b8aa497147eef4403108ad28e8a3188e318
Gabe Kangas <gabek@real-ity.com>
2016-02-11 08:30:42 -0800

    Updated example config file

--- config-example.js
+++ config-example.js
@@ -5,19 +5,33 @@ console.log("Environment: " + env);
 
 config.enableCache = true;
 config.enableImageCache = true;
-config.version = "1.1.3";
-config.useragent = 'TheBatServer ( http://thebatplayer.fm v' + config.version + ')';
+config.version = "1.1.8";
+config.useragent = 'TheBatServer (http://thebatplayer.fm v' + config.version + ')';
 config.cachetime = 5;
-config.hostname = "http://batserver.thebatplayer.fm";
 
-config.discogsAccesskey = "DISCOGS-KEY";
-config.discogsSecret = "DISCOGS-SECRET"
-config.lastfmKey = "LAST-FM-API-KEY";
+config.discogsAccesskey = "DISCOGS-ACCESS-KEY";
+config.discogsSecret = "DISCOGS-SECRET";
+config.lastfmKey = "LASTFM-KEY";
+
+config.gracenoteClientId = "GRACENOTE-CLIENT-ID";
+config.gracenoteClientTag = "GRACENOTE-CLIENT-TAG";
+config.gracenoteUserId = "GRACENOTE-USER-ID";
+
+config.enableAnalytics = true;
+
+config.rollbarKey = "ROLLBARKEY"; // Used for error reporting
+
+// https://www.imgix.com/docs
+config.imgixKey = "IMGXKEY" // Used for image generation
 
 // Override for tests
 if (env === "test") {
   config.enableCache = false;
   config.enableImageCache = false;
+  config.enableAnalytics = false;
 }
 
+// This is the Amazon AWS elasticache autodiscovery server that returns back a memcache instance
+// https://aws.amazon.com/elasticache/faqs/
+config.awsElasticacheConfigServer = "something.like.cfg.usw2.cache.amazonaws.com:11211";
 module.exports = config;
