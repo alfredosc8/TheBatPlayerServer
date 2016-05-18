d779274f508767e0fcac180dc7efcd9e21995079
Gabe Kangas <gabek@real-ity.com>
2016-05-18 09:12:32 -0700

    Add runscope to whitelist

--- utils/userAgentCheck.js
+++ utils/userAgentCheck.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function userAgentCheck(req, res, next) {
-  let whitelist = ["Roku/DVP", "TheBatPlayer/AppleTV", "Chrome"];
+  let whitelist = ["Roku/DVP", "TheBatPlayer/AppleTV", "Chrome", "runscope-radar"];
   let userAgent = req.get('User-Agent');
 
   var matches = whitelist.filter(function(el) {
